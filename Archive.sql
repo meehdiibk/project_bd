b6892b29494fb832ddd7dfbf5f080aba122d6727
armanozyonum <arman@ozyonum.com>
2018-12-04 12:01:23 +0100

    changement archive

--- Archive.sql
+++ Archive.sql
@@ -6,91 +6,87 @@ DROP TABLE IF EXISTS EpreuveCollective cascade;
 DROP TABLE IF EXISTS EpreuveIndividuelle cascade;
 DROP TABLE IF EXISTS Athlete_EpreuveIndividuelle cascade;
 DROP TABLE IF EXISTS Equipe_EpreuveCollective cascade;
-
+DROP TABLE IF EXISTS Resultats_Equipe cascade;
 
 CREATE TABLE Pays (
   id_pays serial,
   nom text not null,
-  nb_medailles_or integer,
-  nb_medailles_bronze integer,
-  nb_medailles_argent integer,
-  rang integer,
-  PRIMARY KEY(nom)
-);
-
-CREATE TABLE Athlete (
-  id_athlete serial,
-  nom text not null,
-  sexe varchar(10),
-  nom_pays text not null,
-  nom_sport text not null,
-  nb_medailles_or integer,
-  nb_medailles_argent integer,
-  nb_medailles_bronze integer,
-  rang integer,
-  PRIMARY KEY(id_athlete),
-  FOREIGN KEY(nom_pays) REFERENCES Pays(nom),
-  FOREIGN KEY(nom_sport) REFERENCES Sport(nom_sport)
-);
-
-CREATE TABLE Equipe (
-  id_equipe serial,
-  nom_pays text not null,
-  nom_sport text not null,
-  PRIMARY KEY(id_equipe),
-  FOREIGN KEY(nom_pays) REFERENCES Pays(nom),
-  FOREIGN KEY(nom_sport) REFERENCES Sport(nom_sport)
-);
+  code_pays text not null unique,
+  nb_medailles_or integer default 0,
+  nb_medailles_argent integer default 0,
+  nb_medailles_bronze integer default 0,
+  nb_medailles_total integer default 0,
+  rang integer default 0,
+  PRIMARY KEY(id_pays));
 
 
 CREATE TABLE Sport (
-  id_sport serial PRIMARY KEY,
+  id_sport serial,
   nom_sport text not null,
-<<<<<<< HEAD
-  type text,
-  
+  categorie text,
+  PRIMARY KEY(id_sport)
 );
-DROP TABLE IF EXISTS EpreuveCollective;
-=======
-  type text);
->>>>>>> fe7b91a60a9b075be7e73a505ce40e77845778d9
+
+
+CREATE TABLE Athlete (
+    id_athlete serial,
+    nom text not null,
+    code_pays text not null,
+    id_sport integer not null,
+    nb_medailles_or integer,
+    nb_medailles_argent integer,
+    nb_medailles_bronze integer,
+    rang integer default 0,
+    PRIMARY KEY(id_athlete),
+    FOREIGN KEY(code_pays) REFERENCES Pays(code_pays),
+    FOREIGN KEY(id_sport) REFERENCES Sport(id_sport)
+  );
+
+
+  CREATE TABLE Equipe (
+    id_equipe serial,
+    code_pays integer not null, --code_pays
+    id_sport integer not null,
+    PRIMARY KEY(id_equipe),
+    FOREIGN KEY(code_pays) REFERENCES Pays(code_pays),
+    FOREIGN KEY(id_sport) REFERENCES Sport(id_sport)
+  );
+
 
 CREATE TABLE EpreuveCollective (
   id_epreuve serial PRIMARY KEY,
-  nom_epreuve text not null,
   id_sport integer not null,
-  FOREIGN KEY(id_sport) REFERENCES Sport(id_sport));
-
-
+  FOREIGN KEY(id_sport) REFERENCES Sport(id_sport)
+);
 
 CREATE TABLE EpreuveIndividuelle (
   id_epreuve serial PRIMARY KEY,
   nom_epreuve text not null,
   id_sport integer not null,
-  FOREIGN KEY(id_sport) REFERENCES Sport(id_sport));
-
-
+  FOREIGN KEY(id_sport) REFERENCES Sport(id_sport)
+);
 
 CREATE TABLE Athlete_EpreuveIndividuelle (
   id_athlete integer not null,
   id_epreuve integer not null,
+  medaille text not null,
   performance text,
   FOREIGN KEY(id_athlete) REFERENCES Athlete(id_athlete),
   FOREIGN KEY(id_epreuve) REFERENCES EpreuveIndividuelle(id_epreuve));
 
-
 CREATE TABLE Equipe_EpreuveCollective (
   id_epreuve integer not null,
-  id_gagnant integer not null,
-  id_perdant integer not null,
-  score text,
+  id_equipe integer not null,
+  medaille text not null,
   FOREIGN KEY(id_epreuve) REFERENCES EpreuveCollective(id_epreuve),
-<<<<<<< HEAD
-  FOREIGN KEY(id_gagnant, id_perdant) REFERENCES Equipe(id_equipe),
-);
+  FOREIGN KEY(id_equipe) REFERENCES Equipe(id_equipe));
 
-insert into Pays values(USA);
-  SELECT * from Pays;
-=======
-  FOREIGN KEY(id_gagnant, id_perdant) REFERENCES Equipe(id_equipe));
->>>>>>> fe7b91a60a9b075be7e73a505ce40e77845778d9
+
+CREATE TABLE Resultats_Equipe(
+	id_epreuve integer not null,
+	id_gagnant integer not null,
+	id_perdant integer not null,
+	score text,
+	FOREIGN KEY(id_epreuve) REFERENCES EpreuveCollective(id_epreuve),
+	FOREIGN KEY(id_perdant) REFERENCES Equipe(id_equipe),
+  FOREIGN KEY(id_gagnant) REFERENCES Equipe(id_equipe));
