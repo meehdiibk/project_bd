ad979ebd84f1cbfa82690d0838b0a3b747ad0a0c
armanozyonum <arman@ozyonum.com>
2018-12-16 10:46:51 +0100

    les reponses

--- Requetes.sql
+++ Requetes.sql
@@ -15,11 +15,22 @@ select * from epreuvecollective e, sport s where e.id_sport=s.id_sport;
 select nom_epreuve as epreuve, min(performance) as meilleur_temps from athlete_epreuveindividuelle ae, epreuveindividuelle e, athlete where e.nom_epreuve like 'marathon%' and e.id_epreuve=ae.id_epreuve and athlete.id_athlete=ae.id_athlete group by nom_epreuve;
 
 --Difficulte **
+-- 1
+select e.nom_epreuve,avg(performance) from athlete_epreuveindividuelle ae, epreuveindividuelle e where e.nom_epreuve like '200m nage libre%' and e.id_epreuve=ae.id_epreuve group by e.id_epreuve;
+-- 2
+select * from pays;
+-- 3
+create view medaille_or as select id_epreuve, nom, a.code_pays from athlete a, athlete_epreuveindividuelle ae where medaille='or' and ae.id_athlete=a.id_athlete;
+create view medaille_argent as select id_epreuve, nom, a.code_pays from athlete a, athlete_epreuveindividuelle ae where medaille='argent' and ae.id_athlete=a.id_athlete;
+select * from medaille_or o left join medaille_argent a on a.id_epreuve=o.id_epreuve;
+drop view medaille_or;
+drop view medaille_argent;
 -- 4
 select * from athlete where nb_medailles_or=0;
 -- 5
 select distinct e.* from epreuveindividuelle e, athlete a, athlete_epreuveindividuelle ae where a.code_pays<>'FRA' and a.id_athlete=ae.id_athlete and ae.id_epreuve=e.id_epreuve;
 -- 6
+select ae.* from athlete_epreuveindividuelle ae, epreuveindividuelle e where e.nom_epreuve like '100m%' and e.id_epreuve=ae.id_epreuve and  performance<'00:00:10';
 
 --Difficulte ***
 -- 4
