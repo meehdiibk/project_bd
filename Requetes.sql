884b50ac6155793f660f8ce2b2ebe03afc6e9cbe
armanozyonum <arman@ozyonum.com>
2018-12-16 13:58:06 +0100

    t

--- Requetes.sql
+++ Requetes.sql
@@ -6,7 +6,7 @@ select * from athlete where code_pays='ITA' and nb_medailles_total<>0;
 -- 2
 SELECT A.nom, A.code_pays, medaille FROM Athlete A, EpreuveIndividuelle EI, Athlete_EpreuveIndividuelle AEI WHERE (nom_epreuve like '100m%' or nom_epreuve like '200m%' or nom_epreuve like '400m%') and EI.id_epreuve=AEI.id_epreuve and A.id_athlete=AEI.id_athlete;
 -- 3
-SELECT a.* FROM Athlete a, Sport s WHERE s.nom_sport='Handball' and a.id_sport=s.id_sport and (DATE_PART('year', '2016-01-01'::date) - DATE_PART('year', a.age::date)<25);
+SELECT a.* FROM Athlete a, Sport s WHERE s.nom_sport='Handball' and s.categorie='Femmes' and  a.id_sport=s.id_sport and (DATE_PART('year', '2016-01-01'::date) - DATE_PART('year', a.age::date)<25);
 -- 4
 select * from athlete a, sport s, athlete_epreuveindividuelle ae, epreuveindividuelle e where a.nom like '%Phelps' and a.id_athlete=ae.id_athlete and ae.id_epreuve=e.id_epreuve and e.id_sport=s.id_sport;
 -- 5
